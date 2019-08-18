a7b745b112e75f6cf3fc6e5a8d3e78aa6c409491
alexcherman <sashacherman@gmail.com>
2019-08-18 16:27:56 +0200

    Add responsive version

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,31 @@
 (function() {
   var controller = new ScrollMagic.Controller();
-  new ScrollMagic.Scene({triggerElement: "#sec1"})
+  new ScrollMagic.Scene({triggerElement: "#header"})
     .setClassToggle("#video1", "visible")
-    .addIndicators()
     .addTo(controller);
   new ScrollMagic.Scene({triggerElement: "#sec2"})
     .setClassToggle("#video2", "visible")
-    .addIndicators()
     .addTo(controller);
   new ScrollMagic.Scene({triggerElement: "#sec3"})
     .setClassToggle("#video3", "visible")
-    .addIndicators()
     .addTo(controller);
   new ScrollMagic.Scene({triggerElement: "#sec4"})
     .setClassToggle("#video4", "visible")
-    .addIndicators()
     .addTo(controller);
+
+})();
+
+(function() {
+  var sidebar = document.getElementById('sidebar');
+  var sidebarClose = document.getElementById('sidebarClose');
+  var sidebarOpen = document.getElementById('sidebarOpen');
+
+  sidebarOpen.addEventListener('click', function() {
+    sidebar.classList.add('visible');
+  })
+
+  sidebarClose.addEventListener('click', function() {
+    sidebar.classList.remove('visible');
+  })
 
 })();
